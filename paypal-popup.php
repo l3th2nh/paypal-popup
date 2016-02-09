71fed71d18a2a02fd5a527e0c7b33a36692b5870
Andrea Cognini <andrea.cognini@triboo.it>
2016-02-09 16:38:15 +0100

    implement plugin meta

--- paypal-popup.php
+++ paypal-popup.php
@@ -1,10 +1,13 @@
 <?php
 /*
 Plugin Name: Paypal Popup
+Plugin URI:  https://github.com/heavybeard/paypal-popup
 Version: 0.0.1
 Description: Wordpress plugin for displaing a Paypal Donate popup
 Author: Heavybeard
 Author URI: http://heavybeard.it
+License: https://github.com/heavybeard/paypal-popup/blob/master/LICENSE
+Text Domain: paypal-popup
 */
 
 require_once 'dist/paypal-popup.php';
