90e72b8fef061474a8d0044e083fc47dddd8b5b4
Andrea Cognini <andrea.cognini@triboo.it>
2016-02-09 16:41:13 +0100

    correct functions for best performance

--- source/script/paypal-popup.js
+++ source/script/paypal-popup.js
@@ -1,122 +1,110 @@
+/* global jQuery:false, console:false, document:false, window:false */
+
 /**
  * PAYPAL POPUP
  * @description Required script for managin popup
  * @see https://github.com/heavybeard/paypal-popup
  */
 
-+function() {
-    'use strict';
+'use strict'; // jshint ignore:line
 
-    /** Global functions */
-    var clickCloseFn,
-        readyOpenFn;
+/**
+ * PAYPALPOPUP
+ * @description Create paypalPopup object
+ */
+function PaypalPopup(elementID) {
+    this._element = {};
+    this._open = false;
+}
 
-    /**
-     * PAYPALPOPUP
-     * @description Create paypalPopup object
-     */
-    function PaypalPopup(elementID) {
-        this._window = window;
-        this._document = document;
-        this._element = {};
+/**
+ * PAYPALPOPUP INIT
+ * @description Initialize
+ * @param string elementID The id element to assign the class
+ */
+PaypalPopup.prototype.init = function (elementID, open) {
+    /** Get the element */
+    this._getElement(elementID);
+    window.paypalPopup1.openPopup();
+
+    this._element.addEventListener('click', this.closePopup.bind(this));
+};
+
+/**
+ * PAYPALPOPUP GET ELEMENT
+ * @description Assign paypalPopup to a DOM element
+ * @param string elementID The id element to assign the class
+ */
+PaypalPopup.prototype._getElement = function (elementID) {
+    /** Stop if parameter is not a string */
+    if (typeof elementID !== 'string')
+        return;
+
+    this._element = document.getElementById(elementID);
+
+    /** Stop if selected element not exist */
+    if (this._element.length <= 1)
+        return;
+};
+
+/**
+ * PAYPALPOPUP SET OPEN
+ * @description For setting popup open status
+ * @param bool open True or False for open or close
+ * @return bool
+ */
+PaypalPopup.prototype._setOpen = function (open) {
+    if (open === true)
+        this._open = true;
+    else
         this._open = false;
-    }
+};
 
-    /**
-     * PAYPALPOPUP INIT
-     * @description Initialize
-     * @param string elementID The id element to assign the class
-     */
-    PaypalPopup.prototype.init = function (elementID, open) {
-        /** Get the element */
-        this._getElement(elementID);
-        this._setOpen(open);
+/**
+ * PAYPALPOPUP IS OPEN
+ * @description For knowing if popup is open
+ * @return bool
+ */
+PaypalPopup.prototype.isOpen = function () {
+    if (this._open === true)
+        return true;
+    else
+        return false;
+};
 
-        clickCloseFn = this.closePopup.bind(this);
-        readyOpenFn = this.openPopup.bind(this);
+/**
+ * PAYPALPOPUP OPEN POPUP
+ * @description Open the popoup
+ */
+PaypalPopup.prototype.openPopup = function() {
+    /** Stop if is open */
+    if (this.isOpen())
+        return;
 
-        /** Add events */
-        this._document.addEventListener('DOMContentLoaded', readyOpenFn);
-        this._element.addEventListener('click', clickCloseFn);
+    /** DOM */
+    this._element.classList.remove('paypal-popup-modal-close');
+    this._element.classList.add('paypal-popup-modal-open');
 
-        /** Close the popup */
-        clickCloseFn();
-    };
+    this._setOpen(true);
+};
 
-    /**
-     * PAYPALPOPUP GET ELEMENT
-     * @description Assign paypalPopup to a DOM element
-     * @param string elementID The id element to assign the class
-     */
-    PaypalPopup.prototype._getElement = function (elementID) {
-        /** Stop if parameter is not a string */
-        if (typeof elementID !== 'string')
-            return;
+/**
+ * PAYPALPOPUP CLOSE POPUP
+ * @description Close the popoup
+ */
+PaypalPopup.prototype.closePopup = function() {
+    /** Stop if is close */
+    if (!this.isOpen())
+        return;
 
-        this._element = this._document.getElementById(elementID);
+    /** DOM */
+    this._element.classList.remove('paypal-popup-modal-open');
+    this._element.classList.add('paypal-popup-modal-close');
 
-        /** Stop if selected element not exist */
-        if (this._element.length <= 1)
-            return;
-    };
+    this._setOpen(false);
+};
 
-    /**
-     * PAYPALPOPUP SET OPEN
-     * @description For setting popup open status
-     * @param bool open True or False for open or close
-     * @return bool
-     */
-    PaypalPopup.prototype._setOpen = function (open) {
-        if (open === true)
-            this._open = true;
-        else
-            this._open = false;
-    };
-
-    /**
-     * PAYPALPOPUP IS OPEN
-     * @description For knowing if popup is open
-     * @return bool
-     */
-    PaypalPopup.prototype.isOpen = function () {
-        if (this._open === true)
-            return true;
-        else
-            return false;
-    };
-
-    /**
-     * PAYPALPOPUP OPEN POPUP
-     * @description Open the popoup
-     */
-    PaypalPopup.prototype.openPopup = function() {
-        /** Stop if is open */
-        if (this.isOpen())
-            return;
-
-        /** DOM */
-        this._element.classList.remove('paypal-popup-close');
-        this._element.classList.add('paypal-popup-open');
-
-        setOpen(true);
-    };
-
-    /**
-     * PAYPALPOPUP CLOSE POPUP
-     * @description Close the popoup
-     */
-    PaypalPopup.prototype.closePopup = function() {
-        /** Stop if is close */
-        if (!this.isOpen())
-            return;
-
-        /** DOM */
-        this._element.classList.remove('paypal-popup-open');
-        this._element.classList.add('paypal-popup-close');
-
-        setOpen(false);
-    };
-
-    var paypalPopup1 = new PaypalPopup().init('cb-lwa-paypal', true);
-
-}(); // jshint ignore:line
+document.addEventListener("DOMContentLoaded", function() {
+    window.paypalPopup1 = new PaypalPopup();
+    window.paypalPopup1.init('paypal-popup', true);
+});
